334e849db354912c4d32673800901a19fc0a88e0
Felipe Porfirio de Lima <157161957+Porfir1o@users.noreply.github.com>
2024-01-22 14:52:57 -0300

    Update dataset.py

--- src/dataset.py
+++ src/dataset.py
@@ -1,12 +1,10 @@
 import json
 import pandas as pd
 
-<<<<<<< HEAD
-file = open("C:\\Users\\Porfirio\\PycharmProjects\\GUPPE\\Dashboard_Vendas\\src\\vendas.json")
-=======
+#file = open("C:\\Users\\Porfirio\\PycharmProjects\\GUPPE\\Dashboard_Vendas\\src\\vendas.json")
 file = open('src/vendas.json')
 #open('C:\\Users\\Porfirio\\PycharmProjects\\GUPPE\\venv\\dados\\vendas.json')
->>>>>>> a48339631f86511cb278f26907d2d1e54d548782
+
 data = json.load(file)
 
 #print(data)
